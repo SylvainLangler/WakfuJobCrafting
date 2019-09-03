a337d20e6826cf6356bec56dcc82c159e4609caa
Sylvain Langler <sylvain.langler@etu.unice.fr>
2019-09-04 01:02:14 +0200

    début mise en page des card pour les items, ajout des items pour maroquinier et tailleur

--- js/app.js
+++ js/app.js
@@ -5,23 +5,26 @@ $(document).ready(function () {
         for(var indexMetier in data.craft.metiers){ // Pour chaque métier
 
             var metier = data.craft.metiers[indexMetier];
-            $(".description").append("<p>M\u00E9tier: "+metier.nom+"</p>"); // On affiche le nom du métier
+            $("."+metier.nom).append("<div class=\"col-lg-12 text-center mb-5 mt-5\"><h3>"+metier.nom+"</h3></div>"); // On affiche le nom du métier
 
             for(var indexItem in metier.items){ // Pour chaque item à craft dans le métier
 
                 var item = metier.items[indexItem];
-                $(".description").append("<p>Objet: "+item.nom+"</p>"); // On affiche le nom de l'item
-                $(".description").append("<p class=\"text-center\">Craft:</p>");
+                var stringItem = "<div class=\"col-lg-3 item\"><div class=\"item-name\"><h5>"+item.nom+"</h5></div><table class=\"table m-auto w-100\"><thead><tr><th scope=\"col\">Nom</th><th scope=\"col\">Quantité</th></tr></thead><tbody>";
 
                 for(var indexRecetteItem in item.items_recette){ // Pour chaque item de la recette de l'item à craft
 
                     var recetteItem = item.items_recette[indexRecetteItem];
-                    $(".description").append("<p>"+recetteItem.nom+" : "+recetteItem.quantite+"</p>"); // On affiche son nom et la quantité
+                    stringItem += "<tr><td>"+recetteItem.nom+"</td><td>"+recetteItem.quantite+"</td>";
 
                 }
+
+                stringItem += "</tbody></table></div>";
+                $("."+metier.nom).append(stringItem);
             }
+            
         }
-        
+
     });
 
 });
